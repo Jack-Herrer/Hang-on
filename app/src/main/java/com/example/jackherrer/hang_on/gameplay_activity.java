ccfcd05dd1951c0538ea336e29311232f2b9465b
Jack-Herrer <michielvanderlist@yahoo.co.uk>
2015-12-04 00:52:18 +0100

    game starts in game, start action bar development

--- app/src/main/java/com/example/jackherrer/hang_on/gameplay_activity.java
+++ app/src/main/java/com/example/jackherrer/hang_on/gameplay_activity.java
@@ -2,6 +2,7 @@ package com.example.jackherrer.hang_on;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class gameplay_activity extends AppCompatActivity {
@@ -14,6 +15,10 @@ public class gameplay_activity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay_activity);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         gameplayclass.initiate_blank_spaces(this);
 
     }
--- app/src/main/java/com/example/jackherrer/hang_on/main_menu.java
+++ app/src/main/java/com/example/jackherrer/hang_on/main_menu.java
@@ -3,6 +3,7 @@ package com.example.jackherrer.hang_on;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class main_menu extends AppCompatActivity {
@@ -11,6 +12,10 @@ public class main_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 
     public void play_click(View view) {
